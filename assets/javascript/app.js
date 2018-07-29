8abde043b3bc74f7b966da7a736eaa91d9b15fec
Jonathan <sellsj14@gmail.com>
2018-07-29 17:33:26 -0400

    the gifs will animate and stop on click now

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -39,29 +39,47 @@ $(document).ready(function(){
             var pull = response.data[i];
             //pull the title
             var gTitle = pull.title;
-            console.log(gTitle);
             //pull the rating of the gif
             var gRating = pull.rating;
-            console.log(gRating);
             //pull the fixed_width_still url of each record (each gif has fixed width of 200px yet varying heights)
             var gStill = pull.images.fixed_width_still.url;
-            console.log(gStill);
             //pull the animated version
             var gAnimated = pull.images.fixed_width.url;
-            console.log(gAnimated);
             //now build the col with the pulled content
             //lock col width and padding down to 220px + 20px padding 
             //make an img tag with src= to the gif url and .gif to be target by an onclick function
-            var gif = $("<img>").attr({src:gStill,"data-animated":gAnimated,"data-still":gStill}).addClass(".gif");
+            //need to add a preFACE url so that the like loads from giphy and not my local index file
+            var gif = $("<img>").attr("src", gStill).attr("data-animated",gAnimated).attr("data-still",gStill).attr("data-state","still").addClass("gif");
+            console.log(gif);
+            console.log(gif[0].baseURI);
             //put the gif into the column
             $(gifHolder).append(gif);
             //put the column into the row #display
             $("#display").append(gifHolder);
-        }
-        //build an onclick selector to make the gifs animate or stop when clicked on
-        
-    });
 
+        }//end loop through records
 
+         //build an onclick selector to make the gifs animate or stop when clicked on
+        $(".gif").on("click",function () {
+            //grab the current data-state of the gif(this is the key to switching the state)
+            var state = $(this).attr("data-state");
+            console.log(state);
+            //if the state is still then switch the src to data-animated, if it is animated src = data-still
+            if(state === "still"){
+                //bind the gifs animated link to a variable
+                $(this).attr("src",$(this).attr("data-animated"));
+                //update the state to animated so it can be stopped next clicks
+                $(this).attr("data-state", "animated")
+            }
+            else{
+                //display the still gif
+                $(this).attr("src",$(this).attr("data-still"));
+                //update the state to animated so it can be animated on next click
+                $(this).attr("data-state", "still");
+            }
+    
+        });//end .gif animate / stop function
 
-});
+    });//end ajax query
+
+});//end document ready function
